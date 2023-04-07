bcce3c87d42fa5ad7ed42cd7f685da49f246b53c
Даниил <dchibitok@yandex.ru>
2023-04-07 15:47:54 +0300

    added new endpoint for mutation

--- src/main/java/com/graphql/api/controllers/ItemController.java
+++ src/main/java/com/graphql/api/controllers/ItemController.java
@@ -1,8 +1,10 @@
 package com.graphql.api.controllers;
 
 import com.graphql.api.models.Item;
-import com.graphql.api.repositories.ItemRepository;
+import com.graphql.api.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -12,15 +14,20 @@ import java.util.List;
 @Component
 @Controller
 public class ItemController {
-    private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @Autowired
-    public ItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
+    public ItemController(ItemService ItemService) {
+        this.itemService = ItemService;
     }
 
     @QueryMapping
     public List<Item> findAllItems() {
-        return itemRepository.findAll();
+        return itemService.findAllItems();
+    }
+
+    @MutationMapping
+    public Item createItem(@Argument Item item) {
+        return itemService.createItem(item);
     }
 }
