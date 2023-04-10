a459f10c647f644ffbdeb5fd588d394fedd7976b
Даниил <dchibitok@yandex.ru>
2023-04-10 14:47:17 +0300

    created entity which describes state for token

---
+++ src/main/java/com/graphql/api/security/jwt/models/StateToken.java
@@ -0,0 +1,24 @@
+package com.graphql.api.security.jwt.models;
+
+import com.graphql.api.security.jwt.models.base.BaseToken;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class StateToken extends BaseToken {
+    @OneToMany(mappedBy = "stateToken")
+    private List<Token> tokens;
+
+    public StateToken(String name) {
+        setName(name);
+    }
+}
