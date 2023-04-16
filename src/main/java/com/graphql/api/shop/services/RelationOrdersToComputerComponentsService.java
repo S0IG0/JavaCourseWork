9111979a9009d397918151d8846185180c9cb1a9
Даниил <dchibitok@yandex.ru>
2023-04-17 00:02:58 +0300

    create RelationOrdersToComputerComponents

---
+++ src/main/java/com/graphql/api/shop/services/RelationOrdersToComputerComponentsService.java
@@ -0,0 +1,7 @@
+package com.graphql.api.shop.services;
+
+import com.graphql.api.shop.models.RelationOrdersToComputerComponents;
+
+public interface RelationOrdersToComputerComponentsService {
+    RelationOrdersToComputerComponents saveRelation(RelationOrdersToComputerComponents relation);
+}
