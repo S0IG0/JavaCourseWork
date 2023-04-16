157e4b8fd77b73118a3bf1f0636f1075c51f49c3
Даниил <dchibitok@yandex.ru>
2023-04-16 23:57:34 +0300

    delete items

--- src/main/java/com/graphql/api/shop/repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.graphql.api.shop.repositories;
-
-import com.graphql.api.shop.models.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
