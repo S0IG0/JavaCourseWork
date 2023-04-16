9b6c47011ea986a910eeefdf81d766bef0437b8b
Даниил <dchibitok@yandex.ru>
2023-04-17 00:04:12 +0300

    uses EnableGlobalMethodSecurity

--- src/main/java/com/graphql/api/security/configs/SecurityConfig.java
+++ src/main/java/com/graphql/api/security/configs/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -29,7 +29,7 @@ import java.util.*;
 @Slf4j
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
@@ -79,6 +79,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/graphql").permitAll()
                         .requestMatchers("/graphiql").permitAll()
+                        .requestMatchers("/images").permitAll()
+                        .requestMatchers("/images/**").permitAll()
                         .anyRequest().authenticated()
                         .and().addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 ).httpBasic().disable();
